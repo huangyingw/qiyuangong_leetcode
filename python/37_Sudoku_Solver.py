3f044307b7bcfb47112dc050b157d12bd65a8a72
qiyuangong <qiyuangong@gmail.com>
2016-04-13 21:09:24 +0800

    37_Sudoku_Solver 42_Trapping_Rain_Water 104_Maximum_Depth_of_Binary_Tree 118_Pascal's_Triangle

--- python/37_Sudoku_Solver.py
+++ python/37_Sudoku_Solver.py
@@ -4,22 +4,40 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: void Do not return anything, modify board in-place instead.
         """
-        pass
-
-    def isValidSudoku(self, board):
-        vset = [0] * 9
-        hset = [0] * 9
-        bset = [0] * 9
+        #https://leetcode.com/discuss/84831/java-backtracking-stack-20ms
+        empty = []
         for i in range(9):
             for j in range(9):
-                curr = board[i][j]
-                if curr != '.':
-                    index = 1 << (ord(curr) - ord('0'))
-                    if (hset[i] & index) > 0 or \
-                                    (vset[j] & index) > 0 or \
-                                    (bset[(i / 3) * 3 + j / 3] & index) > 0:
-                        return False
-                    hset[i] |= index
-                    vset[j] |= index
-                    bset[(i / 3) * 3 + j / 3] |= index
+                if board[i][j] == '.':
+                    empty.append(9 * i + j)
+        self.solve(board, empty)
+
+    def solve(self, board, empty):
+        if len(empty) == 0:
+            return True
+        first_value = empty[-1]
+        row, col = first_value / 9, first_value % 9
+        for k in range(1, 10):
+            if self.is_safe(board, row, col, str(k)):
+                board[row][col] = str(k)
+                empty.pop()
+                if self.solve(board, empty):
+                    return True
+                board[row][col] = '.'
+                empty.append(first_value)
+        return False
+
+    def is_safe(self, board, row, col, ch):
+        for k in range(9):
+            if board[k][col] == ch:
+                return False
+            if board[row][k] == ch:
+                return False
+        start_row, start_col = 3 * (row / 3), 3 * (col / 3)
+        for i in range(start_row, start_row + 3):
+            for j in range(start_col, start_col + 3):
+                if board[i][j] == ch:
+                    return False
         return True
+
+
