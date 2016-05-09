32514fadf4f08efca7f3b9d6d68dcdce9ee83214
qiyuangong <qiyuangong@gmail.com>
2016-05-09 20:33:15 +0800

    Prepare to clear linked list problems.

---
+++ python/206_Reverse_Linked_List.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None:
+            return
+        if
---
+++ python/92_Reverse_Linked_Lis_ II.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseBetween(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+        
