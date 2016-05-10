c4c04c69bf3f87322858f7d6498d0cd65836d596
qiyuangong <qiyuangong@gmail.com>
2016-05-10 16:03:37 +0800

    92_Reverse_Linked_Lis_ II 206_Reverse_Linked_List

--- python/92_Reverse_Linked_Lis_ II.py
+++ python/92_Reverse_Linked_Lis_ II.py
@@ -12,4 +12,29 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
+        if m == n:
+            return head
+        split_node, prev, curr = None, None, head
+        count = 1
+        while count <= m and curr is not None:
+            if count == m:
+                split_node = prev
+            prev = curr
+            curr = curr.next
+            count += 1
+        tail, next_node = prev, None
+        while curr is not None and count <= n:
+            next_temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_temp
+            count += 1
+        if split_node is not None:
+            split_node.next = prev
+        if tail is not None:
+            tail.next = curr
+        if m == 1:
+            return prev
+        return head
+
         
