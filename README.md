835e147b23dc187f7bba8d5e950eeb51b996c088
qiyuangong <qiyuangong@gmail.com>
2016-07-31 03:12:22 +0800

    Add more solution and comments.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# My leetcode solutions with Python and Java
+# My Python Solution for Leetcode
+
+
+| # | Title | Solution | Basic idea (One line) |
+|---| ----- | -------- | ---------- |
+| 1 | [Two Sum](https://leetcode.com/problems/two-sum/) | [Python](https://github.com/qiyuangong/leetcode/blob/master/python/1_Two_Sum.py) | 1. hash 2. Sort and search with two point|
+| 15 | [3Sum](https://leetcode.com/problems/3sum/) | [Python](https://github.com/qiyuangong/leetcode/blob/master/python/15_3Sum.py) | 1. Sort and O(n) search with three points 2. Multiple Two Sum (Problem 1) |
--- python/1_Two_Sum.py
+++ python/1_Two_Sum.py
@@ -34,16 +34,31 @@ class Solution(object):
     #         except KeyError:
     #             pass
 
+    # def twoSum(self, nums, target):
+    #     # hash 2
+    #     hash_nums = {}
+    #     for index, num in enumerate(nums):
+    #         another = target - num
+    #         try:
+    #             hash_nums[another]
+    #             return [hash_nums[another], index]
+    #         except KeyError:
+    #             hash_nums[num] = index
+
     def twoSum(self, nums, target):
-        # hash 2
-        hash_nums = {}
-        for index, num in enumerate(nums):
-            another = target - num
-            try:
-                hash_nums[another]
-                return [hash_nums[another], index]
-            except KeyError:
-                hash_nums[num] = index
+        # two point
+        nums_index = [(v, index)for index, v in enumerate(nums)]
+        nums_index.sort()
+        begin, end = 0, len(nums) - 1
+        while begin < end:
+            curr = nums_index[begin][0] + nums_index[end][0]
+            if curr == target:
+                return [nums_index[begin][1], nums_index[end][1]]
+            elif curr < target:
+                begin +=1
+            else:
+                end -= 1
+
 
 if __name__ == '__main__':
     # begin
