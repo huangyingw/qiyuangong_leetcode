34ae8ff0bbc5cd9006cddb195ff683bcafd61b37
qiyuangong <qiyuangong@gmail.com>
2016-08-23 11:10:15 +0800

    21_Merge_Two_Sorted_Lists 23_Merge_k_Sorted_Lists 138_Copy_List_with_Random_Pointer

--- python/21_Merge_Two_Sorted_Lists.py
+++ python/21_Merge_Two_Sorted_Lists.py
@@ -13,34 +13,23 @@
 #         """
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
-        if l1 is None:
-            return l2
-        elif l2 is None:
-            return l1
-        elif l1 is None and l2 is None:
-            return None
-        res = tail = ListNode(-1)
-        p1, p2 = l1, l2
-        while p1 is not None and p2 is not None:
-            if p1.val <= p2.val:
-                curr = p1.val
-                p1 = p1.next
+        # dummy head
+        pos = dummyHead = ListNode(-1)
+        while l1 is not None and l2 is not None:
+            if l1.val <= l2.val:
+                pos.next = l1
+                l1 = l1.next
             else:
-                curr = p2.val
-                p2 = p2.next
-            tail.next = ListNode(curr)
-            tail = tail.next
-        while p1 is not None:
-            curr = p1.val
-            tail.next = ListNode(curr)
-            tail = tail.next
-            p1 = p1.next
-        while p2 is not None:
-            curr = p2.val
-            tail.next = ListNode(curr)
-            tail = tail.next
-            p2 = p2.next
-        return res.next
+                pos.next = l2
+                l2 = l2.next
+            pos = pos.next
+        # merge residual l1
+        if l1 is not None:
+            pos.next = l1
+        # merge residual l2
+        if l2 is not None:
+            pos.next = l2
+        return dummyHead.next
 
 
     # def mergeTwoLists(self, l1, l2):
