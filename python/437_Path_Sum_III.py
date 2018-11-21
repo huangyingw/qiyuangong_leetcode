c8b108afbcc01c579143feda77e666d433845566
Qiyuan Gong <qiyuan.gong@intel.com>
2018-11-21 22:16:24 +0800

    461_Hamming_Distance and fix links

--- python/437_Path_Sum_III.py
+++ python/437_Path_Sum_III.py
@@ -29,8 +29,10 @@ class Solution(object):
 
     def pathSumHelper(self, root, target, so_far, cache):
         if root:
+            # complement == 1, root->curr path
             complement = so_far + root.val - target
             if complement in cache:
+                # S->E path, sum(root->S)-sum(root->E) = target
                 self.result += cache[complement]
             cache[so_far + root.val] = cache.get(so_far + root.val, 0) + 1
             self.pathSumHelper(root.left, target, so_far + root.val, cache)
