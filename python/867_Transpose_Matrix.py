f96c51209837446033a20b5fd29f39e8e8fc3399
Qiyuan Gong <qiyuan.gong@intel.com>
2018-12-22 22:53:40 +0800

    700_Search_in_a_Binary_Search_Tree

--- python/867_Transpose_Matrix.py
+++ python/867_Transpose_Matrix.py
@@ -10,5 +10,14 @@ class Solution(object):
             for c, val in enumerate(row):
                 ans[c][r] = val
         return ans
-        #Alternative Solution:
-        #return zip(*A)
+        # Alternative Solution:
+        # return zip(*A)
+
+    # def transpose(self, A):
+    #     res = []
+    #     for i in range(len(A[0])):
+    #         temp = []
+    #         for j in range(len(A)):
+    #             temp.append(A[j][i])
+    #         res.append(temp)
+    #     return res
