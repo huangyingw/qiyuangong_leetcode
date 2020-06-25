a2c32380bbd23055ee252109cdc4b700a4530b22
Qiyuan Gong <qiyuan.gong@intel.com>
2020-06-25 22:49:48 +0800

    Remove accu in 1480_Running_Sum_of_1d_Array

--- java/1480_Running_Sum_of_1d_Array.java
+++ java/1480_Running_Sum_of_1d_Array.java
@@ -1,11 +1,8 @@
 class Solution {
     public int[] runningSum(int[] nums) {
         if (nums.length <= 1) return nums;
-        int accu = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            accu += nums[i];
-            nums[i] = accu;
-        }
+        for (int i = 1; i < nums.length; i++)
+            nums[i] += nums[i - 1];
         return nums;
     }
 }
